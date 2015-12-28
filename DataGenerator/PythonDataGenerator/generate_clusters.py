8ff0136543cd140d61021321bb7afe8a1dc4c07a
Aakash Ravi <AakashRavi@Aakashs-MacBook-Pro.local>
2015-12-29 00:51:01 +0100

    Added full initial subspace cluster data generator in DataGenerator/PythonDataGenerator

--- DataGenerator/PythonDataGenerator/generate_clusters.py
+++ DataGenerator/PythonDataGenerator/generate_clusters.py
@@ -1,22 +1,175 @@
 import numpy as np
 import math
+import csv
 import random
 from sklearn.datasets.samples_generator import make_blobs
 
-def GenerateSubspaceCluster(clusters, clustered_dimensions, unclustered_dimensions, amount, 
-    total_deviation, center_vector, unclustered_subspace_range):
+# class DataPointClass:
+# int key - corresponds to the identification of this specific data point.
+# using this key, we can perform an Join on the list of data points to obtain
+# the coordinates of this point
+#
+# string molecule - the specific molecule that this data point belongs to
+#
+# boolean active_flag - 1 or 0 flag depending on whether this point's molecule
+# is an active or inactive
+class DataPointClass:
+    def __init__(self, init_key, init_molecule, active):
+        self.key = int(init_key)
+        self.molecule = init_molecule
+        self.active_flag = active
 
+# Simple syntactic sugar. Just generates an instance of DataPointClass
+# for use in AddClassData
+def PlainDataToClassData(key, molecule_name, active_flag):
+    new_data_with_class = DataPointClass(key, molecule_name, active_flag)
+    return new_data_with_class
+
+# AddClassData
+# all_clusters- an array of clusters, each clusters itself an array of data points
+#
+# active_molecule_list - a list of active molecules to classify the data points within
+# the clusters into
+#
+# inactive_molecule_list - same principle as active_molecule_list but with inactive
+# molecules
+#
+# active_inactive_cluster_ratio - ratio of active vs inactive clusters that the method
+# should initialize. The closer to 1 the ratio the higher percentage of clusters
+# will be clusters of active fragments.
+def AddClassData(all_clusters, active_molecule_list, inactive_molecule_list, 
+    active_inactive_cluster_ratio):
+    
+    # Use the ratio to figure out how many active clusters are needed out of all clusters
+    num_clusters = len(all_clusters)
+    num_active_clusters = int(math.floor(num_clusters * active_inactive_cluster_ratio))
+
+    data_points_with_classes = []
+    
+    # Initialize active clusters, with the corresponding data points in this cluster
+    # being assigned to active molecules
+    for i in range(0, num_active_clusters):
+        current_active_cluster = all_clusters[i]
+
+        for j in range(0, len(current_active_cluster)):
+            random_molecule_index = random.randint(0,len(active_molecule_list)-1)
+
+            class_vector = PlainDataToClassData(current_active_cluster[j][0], \
+                active_molecule_list[random_molecule_index], active_flag=1)
+
+            data_points_with_classes.append(class_vector)
+
+    # Initialize inactive clusters; exactly the same as active clusters, except
+    # we use the remaining clusters from the list of all clusters
+    for i in range(num_active_clusters, len(all_clusters)):
+        current_inactive_cluster = all_clusters[i]
+
+        for j in range(0, len(current_inactive_cluster)):
+            random_molecule_index = random.randint(0,len(inactive_molecule_list)-1)
+
+            class_vector = PlainDataToClassData(current_inactive_cluster[j][0], \
+                inactive_molecule_list[random_molecule_index], active_flag=0)
+            
+            data_points_with_classes.append(class_vector)
+
+    return data_points_with_classes
+
+# Method for flushing the identification key of a data point, and the data point itself
+def FlushData(output_filename, data):
+    # Write the first entry
+    with open(output_filename, 'w') as f_handle:
+        np.savetxt(f_handle, data[0], delimiter=",", fmt="%f")
+    # Append the rest
+    with open(output_filename,'a') as f_handle:
+        for i in range(1, len(data)): 
+            np.savetxt(f_handle, data[i], delimiter=",", fmt="%f")
+
+# Method for flushing the key of a data point along with the specific molecule
+# and activity class that this data point belongs to. Can then do a Join with
+# the raw data points using the identification key and get all the information about
+# a specific point.
+def FlushClassData(output_filename, data):
+    # Write the first entry
+    with open(output_filename, 'w') as f_handle:
+        f_handle.write(str(data[0].key) + ',' + str(data[0].molecule) + ',' + str(data[0].active_flag) + '\n')
+    # Append the rest
+    with open(output_filename, 'a') as f_handle:
+        for i in range(1, len(data)):
+            # f_handle.write("%d,%s,%d,\n" %(data[i].key, data[i].molecule,data[i].active_flag))
+            f_handle.write(str(data[i].key) + "," + str(data[i].molecule) + "," + str(data[i].active_flag) + "\n")
+
+# Method for labelling each raw data point in each cluster with an identification key
+def LabelDataWithKeys(clusters):
+    labelled_clusters = []
+    counter = 0
+
+    for i in range(0, len(clusters)):
+        current_cluster = clusters[i]
+        new_cluster = []
+        for j in range(0, len(current_cluster)):
+            current_vector = current_cluster[j]
+            new_vector = np.insert(current_vector,0,counter)
+            counter = counter +1
+            new_cluster.append(new_vector)
+        labelled_clusters.append(new_cluster)
+    
+    return labelled_clusters
+
+# Generates several subspace clusters, using random seed values for the center
+def GenerateSeveralClusters(clusters, clustered_dimensions, unclustered_dimensions, 
+    amount_of_clusters, points_per_cluster, cluster_radius, unclustered_subspace_range,
+    distance_ratio_between_clusters, max_dimensional_range):
+
+    # Generate a starting cluster center, working as a seed to generate future clusters.
+    center_seed = []
+    random_center_seed = random.randint(0,max_dimensional_range)
+    # Make sure that the clustered dimensions are quite close to eachother
+    for i in range(0, clustered_dimensions):
+        center_seed.append(random_center_seed)
+
+    # Compute the minimum physical distance between clusters, should be the radius of a cluster
+    # multiplied by the distance ration between clusters.
+    inter_cluster_distance = distance_ratio_between_clusters * cluster_radius
+
+    for j in range(1, amount_of_clusters):
+        cluster = []
+        # Generate cluster with a specific center
+        GenerateSubspaceCluster(cluster, clustered_dimensions, unclustered_dimensions,
+            points_per_cluster, cluster_radius, center_seed, unclustered_subspace_range)
+        # Add this cluster to our list of clusters
+        clusters.append(cluster)
+        # Generate a new center for the next cluster, we will shift the center by our
+        # intercluster distance
+        for k in range(0, len(center_seed)):
+            random_shift = random.randint(inter_cluster_distance, max_dimensional_range)
+
+            # Randomly choose positive or negative shift
+            pos_or_neg = random.randint(0,1)
+            if pos_or_neg == 1:
+                random_shift *= -1
+            center_seed[k]+=random_shift
+
+    
+# Generate a single subspace cluster using the center_vector seed and a radius
+# for the cluster
+def GenerateSubspaceCluster(cluster, clustered_dimensions, unclustered_dimensions, points_in_cluster, 
+    cluster_radius, center_seed, unclustered_subspace_range):
+    # Create a local copy of the center_seed since we will be manipulating
+    # the values
+    center_vector = []
+    for i in range(0, len(center_seed)):
+        center_vector.append(center_seed[i])
     #Add some dummy centers for the unclustered dimensions
     for i in range(0, unclustered_dimensions):
         center_vector.append(0)
-    # Total_deviation corresponds to total Euclidean distance from the center.
+    # Cluster_radius corresponds to total Euclidean distance from the center.
     # Therefore we compute the individual deviation for each dimension (assuming the deviation in each
     # dimension is uniform)
-    deviation_squared = math.pow(total_deviation,2)
+    deviation_squared = math.pow(cluster_radius,2)
     deviation_divided_by_number_of_clusterable_dimensions = deviation_squared / clustered_dimensions
     final_deviation_per_dimension = math.sqrt(deviation_divided_by_number_of_clusterable_dimensions) 
 
-    clustered_vectors, labels_true = make_blobs(n_samples=amount, centers=center_vector,
+    clustered_vectors, labels_true = make_blobs(n_samples=points_in_cluster, centers=center_vector,
         cluster_std=final_deviation_per_dimension, random_state=1)
 
     # Now add random noise for the unclustered dimensions
@@ -27,26 +180,7 @@ def GenerateSubspaceCluster(clusters, clustered_dimensions, unclustered_dimensio
             clustered_vectors[i,k] = random_number_in_range
 
         # Add this final vector to the list of points    
-        clusters.append(clustered_vectors[i])
+        cluster.append(clustered_vectors[i])
 
     #Return the cluster for good measure
-    return clusters
-
-def main():
-    clusters = []
-    cluster_center = [5,5,5]
-    num_clustered_dimensions = 3
-    num_unclustered_dimensions = 3
-    amount_of_points = 20
-    total_center_deviation = 4
-    unclustered_noise_range  = 100
-
-    GenerateSubspaceCluster(clusters,num_clustered_dimensions,num_unclustered_dimensions,amount_of_points,
-        total_center_deviation, cluster_center, unclustered_noise_range)
-
-    for j in range(1,len(clusters)):
-        print clusters[j]
-
-if __name__ == '__main__':
-    main()
-
+    return cluster
---
+++ DataGenerator/PythonDataGenerator/sample_usage.py
@@ -0,0 +1,44 @@
+import generate_clusters
+
+def main():
+
+    # Define list of subspace clusters we want
+    clusters = []
+    # Number of clustered dimensions
+    num_clustered_dimensions = 3
+    # Number of unclustered dimensions
+    num_unclustered_dimensions = 3
+    # Amount of clusters 
+    amount_of_clusters = 5
+    # Points per cluster
+    amount_of_points = 20
+    # Radius (or deviation from the center) in each cluster
+    cluster_radius = 4
+    #  Range of noise for the unclustered dimensions
+    unclustered_noise_range = 100
+    # The minimum distance between two clusters
+    distance_ratio_between_clusters = 3
+    # The maximum range of centers for each dimension (centers coordinates
+    # should always stay between 0 and this amount.
+    max_dimensional_range = 200
+
+    generate_clusters.GenerateSeveralClusters(clusters, num_unclustered_dimensions, num_unclustered_dimensions,
+        amount_of_clusters, amount_of_points, cluster_radius, unclustered_noise_range,
+        distance_ratio_between_clusters, max_dimensional_range)
+    
+    # Label the data points in our clusters with identification keys
+    labelled_clusters = generate_clusters.LabelDataWithKeys(clusters)
+    
+    # Flush the laballed clusters
+    generate_clusters.FlushData("labelled_clusters.csv",labelled_clusters)
+
+    # Add class data about the point (the molecule of the point, its activity, etc.)
+    # We would like a ratio of 60% active clusters and 40% inactive clusters
+    data_with_classes = generate_clusters.AddClassData(labelled_clusters, ["C15H20O4","Al2Be3O18Si6"], \
+        ["C6H10O7"," C12H24O2"], .6)
+
+    # Flush the data points
+    generate_clusters.FlushClassData("fragment_data_points.csv", data_with_classes)
+
+if __name__ == '__main__':
+    main()
