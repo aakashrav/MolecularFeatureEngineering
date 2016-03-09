a6d8cc9f3305b05ce7169f64023f947f7a901b9b
Aakash Ravi <aakash_ravi@hotmail.com>
2016-03-09 19:19:16 +0100

    Modified config file to create same base directory for ELKI

--- FeatureExtractor/config.py
+++ FeatureExtractor/config.py
@@ -1,4 +1,4 @@
 import os
 
 DATA_DIRECTORY = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))),"FragmentDescriptorData")
-ELKI_EXECUTABLE = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))),"ELKI/elki-bundle-0.7.0.jar")
+ELKI_EXECUTABLE = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))),"ELKI/elki-bundle-0.7.0.jar")
