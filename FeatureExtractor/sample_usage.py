5d65f2a554df02bf9e55554ae82b90b7bbceb7aa
Aakash Ravi <aakash_ravi@hotmail.com>
2016-04-01 19:58:38 +0200

    added feature normalization

--- FeatureExtractor/sample_usage.py
+++ FeatureExtractor/sample_usage.py
@@ -51,7 +51,7 @@ def main():
     # Find the clusters using ELKI
     molecular_clusters.find_clusters(CLUSTER_FILENAME = os.path.join(config.DATA_DIRECTORY,"detected_clusters"),
         FEATURE_MATRIX_FILE = os.path.join(config.DATA_DIRECTORY,"molecular_feature_matrix.csv"),
-        ELKI_EXECUTABLE=config.ELKI_EXECUTABLE,len(actives),len(inactives),epsilon=None, mu=None)
+        ELKI_EXECUTABLE=config.ELKI_EXECUTABLE,num_active_molecules=len(actives),num_inactive_molecules=len(inactives),epsilon=None, mu=None)
     print "Finished search of molecular clusters"
 
     print "Starting analysis and pruning of found clusters"
