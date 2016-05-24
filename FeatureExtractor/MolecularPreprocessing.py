b1411b9fb2a7dd35dfb14f1eccd9f70527d5a083
Aakash Ravi <aakash_ravi@hotmail.com>
2016-05-24 22:24:24 +0200

    Updated feature removal scheme

--- FeatureExtractor/MolecularPreprocessing.py
+++ FeatureExtractor/MolecularPreprocessing.py
@@ -9,15 +9,12 @@ def _create_feature_matrix(features_file):
 
     with open(features_file,'rb') as input_stream:
 
-        reader = csv.reader(input_stream)
-        # Gets the first line
-        header = next(reader)
+        header = input_stream.readline().rstrip().split(',')
 
-        # for line in input_stream:
-        for line in reader:
-            # line = line.rstrip().split(',')
+        for line in input_stream:
+            line = line.rstrip().split(',')
             name = line[0][1:-1]
-                            
+
             if (feature_matrix is None):
                 feature_matrix = np.empty((0, len(header)-1))
 
@@ -27,16 +24,13 @@ def _create_feature_matrix(features_file):
             # Next we just copy the newly obtained features into our feature matrix
             # If the feature is not a number, we explicitly input np.nan
             # Need the for loop since we may have NaNs
-            for j in range(0, molecule_descriptor_row.shape[1]):
+            for j in range(1, molecule_descriptor_row.shape[1]):
                 try:
                     molecule_descriptor_row[0,j] = float(line[j])
                 except ValueError:
                     molecule_descriptor_row[0,j] = np.nan
-                # except IndexError:
-                #     for k in range(j,molecule_descriptor_row.shape[1]):
-                #         molecule_descriptor_row[0,k] = np.nan
-                #     break
 
+            # Finally append the row to our matrix
             feature_matrix = np.vstack((feature_matrix, molecule_descriptor_row))
     
     feature_matrix = np.hstack((np.asarray(molecule_names).reshape(feature_matrix.shape[0],1),feature_matrix))
@@ -73,10 +67,6 @@ def remove_constant_features(features_file = None, output_features_file = None):
 
     feature_matrix = np.delete(feature_matrix,all_constant_features,1) 
 
-    # for i in range(0,len(feature_matrix[1])-1):
-    #     header_string = str(feature_matrix[1,i]) + ","
-
-
     # Save the data manually, NumPy savetxt seems to be giving problems
     with open(output_features_file,'w+') as f_handle:
         for row in feature_matrix:
@@ -84,14 +74,6 @@ def remove_constant_features(features_file = None, output_features_file = None):
                 f_handle.write(str(row[i])+",")
             f_handle.write(row[len(row)-1])
             f_handle.write("\n")
-
-
-
-    # header_string+=str(feature_matrix[1,feature_matrix.shape[1]-1])
-    # print header_string
-
-    # with open(features_file,"wb+") as f_handle:
-    #     np.savetxt(f_handle, feature_matrix[1:feature_matrix.shape[0]-1], delimiter=',', fmt="%s "+("%f "*(feature_matrix.shape[1]-2))+"%f", header=header_string)
 
 if __name__ == "__main__":
     remove_constant_features(None,None)
