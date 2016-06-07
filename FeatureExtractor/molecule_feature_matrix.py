6f49b8c83b8ac68feb265c02854735e5d206b3b5
Aakash Ravi <aakash_ravi@hotmail.com>
2016-06-07 16:55:00 +0200

    Added correlation neighborhoods

--- FeatureExtractor/molecule_feature_matrix.py
+++ FeatureExtractor/molecule_feature_matrix.py
@@ -112,16 +112,16 @@ def _actives_feature_impute(feature_matrix):
     feature_matrix = np.delete(feature_matrix, degenerate_features, 1)
     print "Actives imputation: removed degenerate features, now have %d features" % (feature_matrix.shape[1])
 
-    # # Compute the significant features using the correlation neighborhoods method
-    # if DESCRIPTOR_TO_RAM:
-    #     neighborhood_extractor.extract_features(NUM_FEATURES,feature_matrix,CORRELATION_THRESHOLD)
+    # Compute the significant features using the correlation neighborhoods method
+    if DESCRIPTOR_TO_RAM:
+        neighborhood_extractor.extract_features(NUM_FEATURES,feature_matrix,CORRELATION_THRESHOLD)
 
-    # significant_features = np.genfromtxt(os.path.join(config.DATA_DIRECTORY,'significant_features'),delimiter=',')
-    # redundant_features = [i for i in range(feature_matrix.shape[1]) if i not in significant_features]
+    significant_features = np.genfromtxt(os.path.join(config.DATA_DIRECTORY,'significant_features'),delimiter=',')
+    redundant_features = [i for i in range(feature_matrix.shape[1]) if i not in significant_features]
 
-    # # Remove the redundant features from the feature matrix
-    # feature_matrix = np.delete(feature_matrix, redundant_features, 1)
-    # print "Actives imputation: removed %d features with constant features and covariance neighborhoods, now have %d features, with the NUM_FEATURES parameters set to %d" % (len(redundant_features), len(significant_features), NUM_FEATURES)
+    # Remove the redundant features from the feature matrix
+    feature_matrix = np.delete(feature_matrix, redundant_features, 1)
+    print "Actives imputation: removed %d features with constant features and covariance neighborhoods, now have %d features, with the NUM_FEATURES parameters set to %d" % (len(redundant_features), len(significant_features), NUM_FEATURES)
 
     # Remove existing dataset files and flush new actives data
     with open(os.path.join(DATA_DIRECTORY,"molecular_feature_matrix.csv"),'w+') as f_handle:
