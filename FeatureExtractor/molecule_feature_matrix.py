e2a0a828ca7c3ef68c2d7704493bedfb8a7da564
Aakash Ravi <aakash_ravi@hotmail.com>
2016-09-28 23:38:18 +0200

    Added more strict epsilons

--- FeatureExtractor/molecule_feature_matrix.py
+++ FeatureExtractor/molecule_feature_matrix.py
@@ -599,8 +599,7 @@ def get_AUC(molecule_names_and_activity, molecules_to_fragments, descriptors_map
                     
                     try:
                         ix_f = descriptors_map[f]
-                        print(ix_f)
-                        descriptors[ix_f]
+                        print(len(descriptors))
                         current_fragment = descriptors[ix_f].reshape(1,len(descriptors[ix_f]))
                         # Append this fragment to our feature matrix
                         feature_matrix = np.vstack((feature_matrix,current_fragment))
