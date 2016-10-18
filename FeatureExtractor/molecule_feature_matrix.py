734ceee87fb89f55eccfd15e08d5f456d1487fd6
Aakash Ravi <aakash_ravi@hotmail.com>
2016-10-18 15:42:12 +0200

    Added New Purity Values

--- FeatureExtractor/molecule_feature_matrix.py
+++ FeatureExtractor/molecule_feature_matrix.py
@@ -1056,7 +1056,7 @@ def main():
                             # for PURITY_CHECK in [True, False]:
 
                             DIVERSITY_THRESHOLD = .5
-                            PURITY_THRESHOLD = .7
+                            PURITY_THRESHOLD = .5
                             PURITY_CHECK = True
 
                             parameter_dictionary = {"DIVERSITY_THRESHOLD":DIVERSITY_THRESHOLD, \
